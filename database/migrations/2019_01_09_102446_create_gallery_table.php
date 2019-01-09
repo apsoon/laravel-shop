0118a5d1c08c5031861995fa8cccf8ed13af198c
wangwenchao <297782066@qq.com>
2019-01-09 18:32:49 +0800

    gallery

---
+++ database/migrations/2019_01_09_102446_create_gallery_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gallery', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("goods_id")->index();
+            $table->string("image_url");
+            $table->integer("sort_order")->default(0);
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gallery');
+    }
+}
