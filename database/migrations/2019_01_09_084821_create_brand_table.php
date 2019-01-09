cd7f1825651ca583616be8073e8e78c57b7fec76
wangwenchao <297782066@qq.com>
2019-01-09 17:05:28 +0800

    brand

---
+++ database/migrations/2019_01_09_084821_create_brand_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name")->default("")->comment("名称");
+            $table->text("describe")->default("")->comment("描述");
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brand');
+    }
+}
