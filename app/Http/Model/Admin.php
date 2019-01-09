3db76c115bf7feb489b9563f925e839b6b4da981
wangwenchao <297782066@qq.com>
2019-01-09 09:54:49 +0800

    change users to admin

--- app/Http/Model/Admin.php
+++ app/Http/Model/Admin.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App;
+namespace App\Http\Model;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use Notifiable;
 
