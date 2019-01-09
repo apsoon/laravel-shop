66a2cccd82a8b7d1e3d99a73e443f7d572f88da2
wangwenchao <297782066@qq.com>
2019-01-09 16:39:21 +0800

    goods relative

---
+++ app/Http/Controllers/Mapi/GoodsMapi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Mapi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GoodsMapi extends Controller
+{
+    //
+}
---
+++ database/migrations/2019_01_09_083543_create_goods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
