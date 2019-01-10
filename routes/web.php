4c6d4df0b06edb2d119ae3f4b35574a896928c2e
wangwenchao <297782066@qq.com>
2019-01-10 11:39:44 +0800

    add admin-lte jquery bootstrap

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,17 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function () {
+    return view('admin.views.admin_template');
+});
+
+Auth::routes();
