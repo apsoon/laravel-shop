246d8004da39179c7dfad5b26b2ecc0087fa8906
wangwenchao <297782066@qq.com>
2019-01-10 15:23:49 +0800

    some sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,11 +1,74 @@
+<!-- inner sidebar -->
 <section class="sidebar">
+    <!-- sidebar menu -->
     <ul class="sidebar-menu">
-        <li><a><i class="fa fa-circle-o"></i>首页</a></li>
-        <li><a><i class="fa fa-circle-o"></i>商品管理</a></li>
-        <li><a><i class="fa fa-circle-o"></i>分类管理</a></li>
-        <li><a><i class="fa fa-circle-o"></i>广告管理</a></li>
-        <li><a><i class="fa fa-circle-o"></i>优惠券管理</a></li>
-        <li><a><i class="fa fa-circle-o"></i>用户管理</a></li>
-        <li><a><i class="fa fa-circle-o"></i>管理员</a></li>
+        <li class="active"><a href="#"><span>系统首页</span></a></li>
+        <li class="treeview">
+            <a href="#"><span>商品管理</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">添加商品</a></li>
+                <li><a href="#">品牌管理</a></li>
+                <li><a href="#">商品分类</a></li>
+                <li><a href="#">品牌管理</a></li>
+            </ul>
+        </li>
+        <li class="treeview">
+            <a href="#"><span>交易管理</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">订单管理</a></li>
+                {{--<li><a href="#">退款管理</a></li>--}}
+                {{--<li><a href="#">物流管理</a></li>--}}
+            </ul>
+        </li>
+        <li class="treeview">
+            <a href="#"><span>优惠券管理</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">优惠券列表</a></li>
+            </ul>
+        </li>
+        <li class="treeview">
+            <a href="#"><span>广告管理</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">广告列表</a></li>
+                <li><a href="#">广告分类</a></li>
+            </ul>
+        </li>
+        <li class="treeview">
+            <a href="#"><span>用户管理</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">用户列表</a></li>
+            </ul>
+        </li>
+        <li class="treeview">
+            <a href="#"><span>管理员</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">管理员列表</a></li>
+                {{--<li><a href="#">权限设置</a></li>--}}
+                {{--<li><a href="#">系统日志</a></li>--}}
+            </ul>
+        </li>
+
+        {{--<li class="treeview">--}}
+        {{--<a href="#"><span>系统管理</span> <i class="fa fa-angle-left pull-right"></i></a>--}}
+        {{--<ul class="treeview-menu">--}}
+        {{--<li><a href="#">系统设置</a></li>--}}
+        {{--</ul>--}}
+        {{--</li>--}}
     </ul>
+    <!-- /.sidebar-menu -->
+
+
+    {{--<ul class="sidebar-menu">--}}
+    {{--<li><a><i class="fa fa-circle-o"></i>系统首页</a></li>--}}
+    {{--<li><a><i class="fa fa-circle-o"></i>商品管理</a></li>--}}
+    {{--<li><a><i class="fa fa-circle-o"></i>分类管理</a></li>--}}
+    {{--<li><a><i class="fa fa-circle-o"></i>广告管理</a></li>--}}
+    {{--<li><a><i class="fa fa-circle-o"></i>优惠券管理</a></li>--}}
+    {{--<li><a><i class="fa fa-circle-o"></i>用户管理</a></li>--}}
+    {{--<li><a><i class="fa fa-circle-o"></i>管理员</a></li>--}}
+    {{--</ul>--}}
 </section>
+
+{{--<div class="main-sidebar">--}}
+
+{{--</div><!-- /.main-sidebar -->--}}
