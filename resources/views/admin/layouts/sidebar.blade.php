13a01cfb15aa9b28f45937094e3acdd0a43ed58d
wangwenchao <297782066@qq.com>
2019-01-11 10:13:57 +0800

    some changes

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,21 +2,21 @@
 <section class="sidebar">
     <!-- sidebar menu -->
     <ul class="sidebar-menu" data-widget="tree">
-        <li class="active"><a href="#"><span>系统首页</span></a></li>
-        <li class="treeview">
+        <li><a href="#"><span>系统首页</span></a></li>
+        <li class="treeview active menu-open">
             <a href="#"><span>商品管理</span> <i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="{{ url("goods/list") }}">商品列表</a></li>
-                <li><a href="{{ url("category/list") }}">商品分类</a></li>
-                <li><a href="{{ url("brand/list") }}">品牌管理</a></li>
-                <li><a href="{{ url("specification/list") }}">商品规格</a></li>
-                <li><a href="{{ url("attribute/list") }}">商品属性</a></li>
+                <li class="active"><a href="{{ url("goods/list") }}"><i class="fa fa-circle-o"></i>商品列表</a></li>
+                <li><a href="{{ url("category/list") }}"><i class="fa fa-circle-o"></i>商品分类</a></li>
+                <li><a href="{{ url("brand/list") }}"><i class="fa fa-circle-o"></i>品牌管理</a></li>
+                <li><a href="{{ url("specification/list") }}"><i class="fa fa-circle-o"></i>商品规格</a></li>
+                <li><a href="{{ url("attribute/list") }}"><i class="fa fa-circle-o"></i>商品属性</a></li>
             </ul>
         </li>
         <li class="treeview">
             <a href="#"><span>交易管理</span> <i class="fa fa-angle-left pull-right"></i></a>
             <ul class="nav treeview-menu menu-open">
-                <li><a href="#">订单管理</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>订单管理</a></li>
                 {{--<li><a href="#">退款管理</a></li>--}}
                 {{--<li><a href="#">物流管理</a></li>--}}
             </ul>
@@ -24,26 +24,26 @@
         <li class="treeview">
             <a href="#"><span>优惠券管理</span> <i class="fa fa-angle-left pull-right"></i></a>
             <ul class="nav treeview-menu menu-open">
-                <li><a href="#">优惠券列表</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>优惠券列表</a></li>
             </ul>
         </li>
         <li class="treeview">
             <a href="#"><span>广告管理</span> <i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="#">广告列表</a></li>
-                <li><a href="#">广告分类</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>广告列表</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>广告分类</a></li>
             </ul>
         </li>
         <li class="treeview">
             <a href="#"><span>用户管理</span> <i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="#">用户列表</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>用户列表</a></li>
             </ul>
         </li>
         <li class="treeview">
             <a href="#"><span>管理员</span> <i class="fa fa-angle-left pull-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="#">管理员列表</a></li>
+                <li><a href="#"><i class="fa fa-circle-o"></i>管理员列表</a></li>
                 {{--<li><a href="#">权限设置</a></li>--}}
                 {{--<li><a href="#">系统日志</a></li>--}}
             </ul>
