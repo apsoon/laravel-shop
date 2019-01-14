404e35e18d5c94ed04095a9fbc639a2fd6945f8e
wangwenchao <297782066@qq.com>
2019-01-14 19:56:11 +0800

    add some changes

--- resources/views/admin/pages/goods/goods_add.blade.php
+++ resources/views/admin/pages/goods/goods_add.blade.php
@@ -8,68 +8,56 @@
                 <form role="form" action="{{url("goods/create")}}" method="post">
                     {!! csrf_field() !!}
                     <div class="form-group">
-                        <label>商品名称</label>
+                        <label>商品名称<b class="text-red">*</b></label>
                         <input class="form-control" id="goods-name" name="name" placeholder="请输入商品名称">
                     </div>
                     <div class="form-group">
                         <label>简要描述</label>
                         <input class="form-control" id="goods-id" name="id" placeholder="请输入简要描述">
                     </div>
-                    <div class="form-group">
-                        <label>商品原价</label>
-                        <input class="form-control" id="goods-id" name="id" placeholder="请输入品牌编号">
+                    <div class="row">
+                        <div class="form-group col-md-3">
+                            <label>商品原价</label>
+                            <input class="form-control" type="number" id="goods-id" name="id" placeholder="请输入商品编号">
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label>商品现价<b class="text-red">*</b></label>
+                            <input class="form-control" type="number" id="goods-id" name="id" placeholder="请输入商品现价">
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>商品数量<b class="text-red">*</b></label>
+                            <input class="form-control" type="number" id="goods-id" name="id" placeholder="请输入商品数量">
+                        </div>
+                        <div class="form-group  col-md-3">
+                            <label>数量单位<b class="text-red">*</b></label>
+                            <input class="form-control" id="gooods-id" name="id" placeholder="请输入数量单位">
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label>商品现价</label>
-                        <input class="form-control" id="goods-id" name="id" placeholder="请输入品牌编号">
+                        <label>选择分类<b class="text-red">*</b></label>
+                        <select></select>
                     </div>
                     <div class="form-group">
-                        <label>商品数量</label>
-                        <input class="form-control" id="goods-id" name="id" placeholder="请输入品牌编号">
+                        <label>选择品牌<b class="text-red">*</b></label>
+                        <select></select>
                     </div>
                     <div class="form-group">
-                        <label>数量单位</label>
-                        <input class="form-control" id="gooods-id" name="id" placeholder="请输入品牌编号">
-                    </div>
-
-                    <div class="form-group">
-                        <label>选择分类</label>
-                        <input class="form-control" id="goods-region" name="region" placeholder="请输入品牌所属地区">
+                        <label>添加图片<b class="text-red">*</b></label>
+                        <input type="file" id="logo" accept="image/gif, image/png, image/jpeg, image/jpg" name="cover">
+                        <p class="help-block">图片大小120px*60px 小于5M</p>
                     </div>
                     <div class="form-group">
-                        <label>选择品牌</label>
-                        <input class="form-control" id="goods-region" name="region" placeholder="请输入品牌所属地区">
-                    </div>
-                    <div class="form-group">
-                        <label>添加图片</label>
-                        <input class="form-control" id="gooods-region" name="region" placeholder="请输入品牌所属地区">
-                    </div>
-                    <div class="form-group">
-                        <label>商品状态</label>
-                        <input class="form-control" id="goods-region" name="region" placeholder="请输入品牌所属地区">
-                    </div>
-                    <div class="form-group">
-                        <label>商品描述</label>
+                        <label>商品描述<b class="text-red">*</b></label>
                         <div id="editor">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="brand-logo">品牌商标</label>
-                        <input type="file" id="logo" name="logo">
-                        <p class="help-block">图片大小120px*60px 小于5M</p>
-                    </div>
-                    <!-- radio -->
-                    <div class="form-group">
-                        <label>
-                            <div class="radio">
-                                <input type="radio" name="state" id="state-on" value="1" checked>
-                                开启
-                            </div>
-                        </label><label>
-                            <div class="radio">
-                                <input type="radio" name="state" id="state-off" value="0">
-                                关闭
-                            </div>
+                        <label>商品状态</label>
+                        <label class="col-md-1">
+                            <input type="radio" name="state" id="state-on" value="1" checked>开启
+                        </label>
+                        <label class="col-md-1">
+                            <input type="radio" name="state" id="state-off" value="0">关闭
                         </label>
                     </div>
                     <div class="box-footer">
