5c657306bd54bb17916893e31444fd119a771181
wangwenchao <297782066@qq.com>
2019-01-14 14:42:01 +0800

    some changes

--- resources/views/admin/pages/goods/attribute_list.blade.php
+++ resources/views/admin/pages/goods/attribute_list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="col-xs-12">
-            <div class="box">
+            <div class="box table-striped">
                 <div class="box-header">
                     <h3 class="box-title">属性列表</h3>
                 </div>
@@ -11,12 +11,19 @@
                     <table class="table table-hover">
                         <tr>
                             <th>名称</th>
+                            <th>状态</th>
                             <th>操作</th>
                         </tr>
                         @foreach($attributes as $attribute)
                             <tr role="row">
                                 <td class="sorting_1">{{ $attribute->name }}</td>
-                                <td>编辑 删除</td>
+                                <td>
+                                    {{--<div class="btn-group">--}}
+                                    <button type="button" class="btn btn-info btn-flat">修改</button>
+                                    {{--<button type="button" class="btn btn-warning  btn-flat">下架</button>--}}
+                                    <button type="button" class="btn btn-danger  btn-flat">删除</button>
+                                    {{--</div>--}}
+                                </td>
                             </tr>
                         @endforeach
                     </table>
