f3cc294d82096a18af0de000b8e5f9d038b69cb7
wangwenchao <297782066@qq.com>
2019-01-15 09:38:55 +0800

    some changes

--- resources/views/admin/pages/goods/attribute_add.blade.php
+++ resources/views/admin/pages/goods/attribute_add.blade.php
@@ -2,22 +2,51 @@
 @section('title', '添加属性')
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        You are logged in!
-                    </div>
-                </div>
-            </div>
-        </div>
+        {{--<div class="box box-primary">--}}
+            {{--<div class="box-body">--}}
+                {{--<form role="form" action="{{url("brand/create")}}" method="post">--}}
+                    {{--{!! csrf_field() !!}--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label>品牌名称</label>--}}
+                        {{--<input class="form-control" id="brand-name" name="name" placeholder="请输入品牌名称">--}}
+                    {{--</div>--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label>品牌编号</label>--}}
+                        {{--<input class="form-control" id="brand-id" name="id" placeholder="请输入品牌编号">--}}
+                    {{--</div>--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label>所属地区</label>--}}
+                        {{--<input class="form-control" id="brand-region" name="region" placeholder="请输入品牌所属地区">--}}
+                    {{--</div>--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label>品牌描述</label>--}}
+                        {{--<textarea class="form-control" id="brand-desc" name="desc" rows="3"--}}
+                                  {{--placeholder="请输入商品描述"></textarea>--}}
+                    {{--</div>--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label for="brand-logo">品牌商标</label>--}}
+                        {{--<input type="file" id="logo" name="logo">--}}
+                        {{--<p class="help-block">图片大小120px*60px 小于5M</p>--}}
+                    {{--</div>--}}
+                    {{--<!-- radio -->--}}
+                    {{--<div class="form-group">--}}
+                        {{--<label>--}}
+                            {{--<div class="radio">--}}
+                                {{--<input type="radio" name="state" id="state-on" value="1" checked>--}}
+                                {{--开启--}}
+                            {{--</div>--}}
+                        {{--</label><label>--}}
+                            {{--<div class="radio">--}}
+                                {{--<input type="radio" name="state" id="state-off" value="0">--}}
+                                {{--关闭--}}
+                            {{--</div>--}}
+                        {{--</label>--}}
+                    {{--</div>--}}
+                    {{--<div class="box-footer">--}}
+                        {{--<button type="submit" id="brand-submit" class="btn btn-primary">提交</button>--}}
+                    {{--</div>--}}
+                {{--</form>--}}
+            {{--</div>--}}
+        {{--</div>--}}
     </div>
 @endsection
