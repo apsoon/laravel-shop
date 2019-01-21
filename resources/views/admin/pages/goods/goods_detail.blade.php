0acf1f1f823d874b55c6ca30993f53a550af85aa
wangwenchao <297782066@qq.com>
2019-01-21 17:11:35 +0800

    front-end code changes

--- resources/views/admin/pages/goods/goods_detail.blade.php
+++ resources/views/admin/pages/goods/goods_detail.blade.php
@@ -23,119 +23,97 @@
                     <div class="tab-pane" id="detail-pane"></div>
                     <!-- 产品列表 -->
                     <div class="tab-pane" id="product-pane">
-                        <div class="box box-primary">
-                            <div class="box-header with-border">
-                                <h3 class="box-title">产品列表</h3>
-                                <a href=" {{url("product/add?goods_id=")}}{{$detail->id}}&category_id={{$detail->category_id}}">
-                                    <button class="btn btn-info btn-flat pull-right">添加产品</button>
-                                </a>
-                            </div>
-                            <div class="box-body">
-                                <div class="col-sm-12 table-responsive">
-                                    <table class="table table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th>产品名称</th>
-                                            <th>规格参数</th>
-                                            <th>原价</th>
-                                            <th>现价</th>
-                                            <th>状态</th>
-                                            <th style="width: 270px">操作</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($productList as $product)
-                                            <tr>
-                                                <td>
-                                                    {{$product->name}}
-                                                </td>
-                                                <td>
-
-                                                </td>
-                                                <td>
-                                                    {{$product->origin_price}}
-                                                </td>
-                                                <td>
-                                                    {{$product->price}}
-                                                </td>
-                                                <td>
-                                                    {{$product->state}}
-                                                </td>
-                                                <td>
-                                                    <button type="button" class="btn btn-success btn-flat">详情
-                                                    </button>
-                                                    <button type="button" class="btn btn-info btn-flat">修改</button>
-                                                    <button type="button" class="btn btn-warning  btn-flat">下架
-                                                    </button>
-                                                    <button type="button" class="btn btn-danger  btn-flat">删除
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                            <div class="box-footer">
-                            </div>
+                        <div class="box-header with-border">
+                            <h3 class="box-title">产品列表</h3>
+                            <a href=" {{url("product/add?goods_id=")}}{{$detail->id}}&category_id={{$detail->category_id}}">
+                                <button class="btn btn-info btn-flat pull-right">添加产品</button>
+                            </a>
                         </div>
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>产品名称</th>
+                                <th>规格参数</th>
+                                <th>原价</th>
+                                <th>现价</th>
+                                <th>状态</th>
+                                <th style="width: 270px">操作</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($productList as $product)
+                                <tr>
+                                    <td>
+                                        {{$product->name}}
+                                    </td>
+                                    <td>
+
+                                    </td>
+                                    <td>
+                                        {{$product->origin_price}}
+                                    </td>
+                                    <td>
+                                        {{$product->price}}
+                                    </td>
+                                    <td>
+                                        {{$product->state}}
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-success btn-flat">详情
+                                        </button>
+                                        <button type="button" class="btn btn-info btn-flat">修改</button>
+                                        <button type="button" class="btn btn-warning  btn-flat">下架
+                                        </button>
+                                        <button type="button" class="btn btn-danger  btn-flat">删除
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                     <!-- 轮播图 -->
                     <div class="tab-pane" id="banner-pane">
-                        <div class="box table-primary">
-                            <div class="box-header with-border">
-                                <h3 class="box-title">轮播图</h3>
-                                <a href=" {{url("product/add?goods_id=")}}{{$detail->id}}&category_id={{$detail->category_id}}">
-                                    <button class="btn btn-info btn-flat pull-right">添加图片</button>
-                                </a>
-                            </div>
-                            <div class="box-body">
-                                <div class="col-sm-12 table-responsive">
-                                    <table class="table table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th style="width: 250px;">图片名称</th>
-                                            <th>图片地址</th>
-                                            <th style="width: 250px;">排序</th>
-                                            <th style="width: 270px">操作</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($productList as $product)
-                                            <tr>
-                                                <td>
-                                                    {{$product->name}}
-                                                </td>
-                                                <td>
-
-                                                </td>
-                                                <td>
-                                                    {{$product->origin_price}}
-                                                </td>
-                                                <td>
-                                                    {{$product->price}}
-                                                </td>
-                                                <td>
-                                                    {{$product->state}}
-                                                </td>
-                                                <td>
-                                                    <button type="button" class="btn btn-success btn-flat">详情
-                                                    </button>
-                                                    <button type="button" class="btn btn-info btn-flat">修改</button>
-                                                    <button type="button" class="btn btn-warning  btn-flat">下架
-                                                    </button>
-                                                    <button type="button" class="btn btn-danger  btn-flat">删除
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                            <div class="box-footer">
-                            </div>
+                        <div class="box-header with-border">
+                            <h3 class="box-title">轮播图</h3>
+                            <a href=" {{url("product/add?goods_id=")}}{{$detail->id}}&category_id={{$detail->category_id}}">
+                                <button class="btn btn-info btn-flat pull-right">添加图片</button>
+                            </a>
                         </div>
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th style="width: 200px;">图片名称</th>
+                                <th>图片地址</th>
+                                <th style="width: 150px;">排序</th>
+                                <th style="width: 270px">操作</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($productList as $product)
+                                <tr>
+                                    <td>
+                                        {{$product->name}}
+                                    </td>
+                                    <td>
+                                        {{$product->origin_price}}
+                                    </td>
+                                    <td>
+                                        {{$product->price}}
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-success btn-flat">详情
+                                        </button>
+                                        <button type="button" class="btn btn-info btn-flat">修改</button>
+                                        <button type="button" class="btn btn-warning  btn-flat">下架
+                                        </button>
+                                        <button type="button" class="btn btn-danger  btn-flat">删除
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
