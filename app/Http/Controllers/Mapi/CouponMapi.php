0b7242c0de44bfd6afc511febaa6d36ffe6c440f
wangwenchao <297782066@qq.com>
2019-01-23 19:20:26 +0800

    coupon

---
+++ app/Http/Controllers/Mapi/CouponMapi.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangwenchao
+ * Date: 2019/1/23
+ * Time: 19:03
+ */
+
+namespace App\Http\Controllers\Mapi;
+
+
+class CouponMapi
+{
+
+}
---
+++ app/Http/Dao/CouponDao.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangwenchao
+ * Date: 2019/1/23
+ * Time: 19:03
+ */
+
+namespace App\Http\Dao;
+
+
+use App\Http\Model\Coupon;
+
+class CouponDao
+{
+    /**
+     * @var Coupon
+     */
+    private $coupon;
+
+    /**
+     * 添加
+     *
+     * @param Coupon $coupon
+     * @return bool
+     */
+    public function insert(Coupon $coupon)
+    {
+        $result = $coupon->save();
+        return $result;
+    }
+
+    /**
+     * id获取
+     *
+     * @param int $id
+     * @return mixed
+     */
+    public function findById(int $id)
+    {
+        $result = $this->coupon::where(["id" => $id])
+            ->get();
+        return $result;
+    }
+
+    /**
+     * 优惠码获取
+     *
+     * @param int $code
+     * @return mixed
+     */
+    public function findByCode(int $code)
+    {
+        $result = $this->coupon::where(["code" => $code])
+            ->first();
+        return $result;
+    }
+
+    /**
+     * CouponDao constructor.
+     *
+     * @param Coupon $coupon
+     */
+    public function __construct(Coupon $coupon)
+    {
+        $this->coupon = $coupon;
+    }
+}
