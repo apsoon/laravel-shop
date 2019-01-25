47763dad53020fe59f5c656e4567fafe9dd628d3
wangwenchao <297782066@qq.com>
2019-01-25 19:46:48 +0800

    sidebar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,22 +1,100 @@
 <template>
     <div>
-        <router-view></router-view>
+        <el-container>
+            <el-header style="background-color: #518CB8">Header</el-header>
+            <el-container>
+                <!--<el-aside width="200px" style="height:100%; background-color: #242C31; position: fixed;">-->
+                <div class="sidebar">
+                    <el-menu class="sidebar-el-menu" unique-opened="true" background-color="#242C31"
+                             text-color="#8598A2"
+                             active-text-color="#D1D3D4" router="true" default-active="1">
+                        <el-menu-item index="1">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">首页</span>
+                        </el-menu-item>
+                        <el-submenu index="2">
+                            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+                            <el-menu-item-group>
+                                <template slot="title">分组一</template>
+                                <el-menu-item index="1-1">选项1</el-menu-item>
+                                <el-menu-item index="1-2">选项2</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                                <el-menu-item index="1-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="1-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-submenu index="3">
+                            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+                            <el-menu-item-group>
+                                <template slot="title">分组一</template>
+                                <el-menu-item index="2-1">选项1</el-menu-item>
+                                <el-menu-item index="2-2">选项2</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                                <el-menu-item index="2-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="2-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-submenu index="4">
+                            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+                            <el-menu-item-group>
+                                <template slot="title">分组一</template>
+                                <el-menu-item index="3-1">选项1</el-menu-item>
+                                <el-menu-item index="3-2">选项2</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                                <el-menu-item index="3-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="3-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                    </el-menu>
+                </div>
+                <!--</el-aside>-->
+                <el-container>
+                    <el-main>
+                        <router-view></router-view>
+                    </el-main>
+                    <el-footer>Footer</el-footer>
+                </el-container>
+            </el-container>
+        </el-container>
     </div>
 </template>
 <script scoped>
 
     export default {
-        data(){
+        data() {
             return {}
         },
-        components: {
-
-        },
+        components: {},
         computed: {},
-        methods: {
-
-        },
+        methods: {},
         mounted() {
         },
     }
 </script>
+<style scoped>
+    .sidebar-el-menu:not(.el-menu--collapse) {
+        width: 250px;
+    }
+
+    .sidebar {
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 70px;
+        bottom: 0;
+        overflow-y: scroll;
+        background: #242C31;
+    }
+</style>
---
+++ resources/js/components/layouts/Siderbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sidebar">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Siderbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
