4c20382fad0c6a3254b95fbf34e26794f575cf0b
wangwenchao <297782066@qq.com>
2019-02-15 16:26:17 +0800

    some changes

--- resources/js/components/layouts/Sidebar.vue
+++ resources/js/components/layouts/Sidebar.vue
@@ -21,11 +21,11 @@
                 <template slot="title"><i class="el-icon-menu"></i>交易管理</template>
                 <el-menu-item index="order-list">订单管理</el-menu-item>
             </el-submenu>
-            <el-submenu index="coupon">
+            <el-submenu index="market">
                 <template slot="title"><i class="el-icon-setting"></i>营销管理</template>
                 <el-menu-item index="coupon-list">优惠券管理</el-menu-item>
             </el-submenu>
-            <el-submenu index="coupon">
+            <el-submenu index="user">
                 <template slot="title"><i class="el-icon-setting"></i>用户管理</template>
                 <el-menu-item index="user-list">用户列表</el-menu-item>
                 <el-menu-item index="addr-list">用户地址</el-menu-item>
