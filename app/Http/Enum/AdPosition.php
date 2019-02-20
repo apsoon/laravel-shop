0ff3defae2d1a6ff522330a4124922b2735295bc
wangwenchao <297782066@qq.com>
2019-02-20 09:25:35 +0800

    adposition

---
+++ app/Http/Enum/AdPosition.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangwenchao
+ * Date: 2019/2/19
+ * Time: 16:24
+ */
+
+namespace App\Http\Enum;
+
+
+/**
+ * 广告位置
+ *
+ * @package App\Http\Enum
+ */
+class AdPosition
+{
+    const BANNER = "banner"; // banner
+}
