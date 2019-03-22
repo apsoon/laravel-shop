bdd9d739564765ef233cf9bbcaca1c43e8836e3a
wangwenchao <297782066@qq.com>
2019-03-22 19:45:57 +0800

    new images

--- app/Http/Controllers/Mapi/UploadMapi.php
+++ app/Http/Controllers/Mapi/UploadMapi.php
@@ -24,7 +24,7 @@ class UploadMapi extends Controller
      * @param Request $request
      * @return JsonResult
      */
-    public function image(Request $request)
+    public function image1(Request $request)
     {
         $req = $request->all();
         $file = $request->file("file");
@@ -49,6 +49,38 @@ class UploadMapi extends Controller
             return new JsonResult(StatusCode::SUCCESS, $result);
         }
         return new JsonResult(StatusCode::SERVER_ERROR);
+    }
+
+    public function image(Request $request)
+    {
+        try {
+
+            $req = $request->all();
+            $file = $request->file("file");
+            if (!$file->isValid()) {
+                return new JsonResult(StatusCode::INVALID_FILE);
+            }
+            // 文件扩展名
+            $extension = $file->getClientOriginalExtension();
+            // 文件名
+            $fileName = $file->getClientOriginalName();
+            // 生成新的统一格式的文件名
+            $newFileName = md5($fileName . time() . mt_rand(1, 10000)) . '.' . $extension;
+            // 图片保存路径
+            $filePath = 'images/' . $req["type"] . "/" . $req["position"].'/';
+            $savePath = public_path() . '/' . $filePath;
+            $file->move($savePath, $newFileName);
+            $result = new \stdClass();
+            $filePath = $filePath . $fileName;
+            $result->fileUrl = asset($filePath);
+            $result->filePath = $filePath;
+            $result->fileName = $newFileName;
+            return new JsonResult(StatusCode::SUCCESS, $result);
+        } catch (\Exception $e) {
+            Log::error(" [ UploadMapi ] ================= image >>>>> error happened when upload a image >>>>> e = ");
+            Log::error(json_encode($e));
+            return new JsonResult(StatusCode::SERVER_ERROR);
+        }
     }
 
     /**
